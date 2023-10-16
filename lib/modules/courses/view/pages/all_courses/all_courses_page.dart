a3589ad28e67c57cc1cc2e4aad0a6921f3b149e6
João Victor Garcia <jvictor_sk8@hotmail.com>
2023-10-16 14:37:06 -0300

    estrutura base do projeto

---
+++ lib/modules/courses/view/pages/all_courses/all_courses_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AllCoursesPage extends StatefulWidget {
+  const AllCoursesPage({super.key});
+
+  @override
+  State<AllCoursesPage> createState() => _AllCoursesPageState();
+}
+
+class _AllCoursesPageState extends State<AllCoursesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/modules/courses/view/pages/course/course_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CoursePage extends StatefulWidget {
+  const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
